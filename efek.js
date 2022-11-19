5572dcd7717668e418547d56b3aaa8f0ac4ae122
AL FANSYAH <43834257+harsanur212@users.noreply.github.com>
2022-11-19 10:21:34 +0700

    upload

---
+++ efek.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#tombol').click(function(){
+        $('.utama').hide("fast");
+        $('.tab2').show("slow");
+        $('.tab3').show("slow");
+
+    })
+})
